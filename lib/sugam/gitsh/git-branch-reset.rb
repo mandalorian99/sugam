6a6287c2310b526f99dce623c10a731b36e77424
Mahendra Choudhary <mahendrachoudhary1156@gmail.com>
2024-09-15 12:05:51 +0530

    minor improvements

--- lib/sugam/gitsh/git-branch-reset.rb
+++ lib/sugam/gitsh/git-branch-reset.rb
@@ -2,44 +2,76 @@
 require 'optparse'
 require 'open3'
 
-def run_git_command(command)
-  output, status = Open3.capture2e(command)
-  unless status.success?
-    puts "Error running command: #{command}\n#{output}"
+# Function to run a Git command and handle errors
+def run_command(command)
+  puts "Running command: #{command}"
+  stdout, stderr, status = Open3.capture3(command)
+  if status.success?
+    puts stdout
+  else
+    puts "Error: #{stderr}"
     exit(1)
   end
-  output
 end
 
-# Function to delete and recreate a branch
-def reset_branch(branch_name)
-  if branch_name == 'master' || branch_name == 'main'
-    puts "Warning: Deleting the #{branch_name} branch is not allowed!"
+# Checkout to master or main branch
+def checkout_to_main_or_master
+  branches = ['master', 'main']
+  branch = branches.find { |b| system("git show-ref --quiet refs/heads/#{b}") }
+
+  if branch
+    run_command("git checkout #{branch}")
+  else
+    puts "Error: Neither 'master' nor 'main' branch exists. Exiting..."
+    exit(1)
+  end
+end
+
+# Stash local changes
+def stash_local_changes
+  run_command("git stash")
+end
+
+# Reset the specified branch
+def reset_branch(branch)
+  if branch == 'master' || branch == 'main'
+    puts "Error: Cannot reset the 'master' or 'main' branch. Exiting..."
     exit(1)
   end
 
-  puts "Resetting #{branch_name} branch..."
-  run_git_command("git branch -D #{branch_name}")
-  run_git_command("git checkout -b #{branch_name} origin/master")
-  puts "#{branch_name} branch has been reset successfully."
+  # Delete the branch and recreate it from the current main/master branch
+  run_command("git branch -D #{branch}") if system("git show-ref --quiet refs/heads/#{branch}")
+  run_command("git checkout -b #{branch}")
 end
 
-# Parse command-line options
+# Main script execution
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: git branch-reset [--qa | --staging]"
+  opts.banner = "Usage: git-branch-reset --qa or --staging"
 
-  opts.on('--qa', 'Reset the QA branch') do
+  opts.on('--qa', 'Reset QA branch') do
     options[:branch] = 'qa'
   end
 
-  opts.on('--staging', 'Reset the Staging branch') do
+  opts.on('--staging', 'Reset Staging branch') do
     options[:branch] = 'staging'
+  end
+
+  opts.on_tail('-h', '--help', 'Show this message') do
+    puts opts
+    exit
   end
 end.parse!
 
-if options[:branch]
-  reset_branch(options[:branch])
-else
-  puts "Please specify a branch to reset with --qa or --staging"
+# Ensure the branch is specified
+unless options[:branch]
+  puts "Error: Please specify a branch using --qa or --staging"
+  exit(1)
 end
+
+# Perform the operations
+checkout_to_main_or_master
+stash_local_changes
+reset_branch(options[:branch])
+
+puts "#{options[:branch]} branch reset successfully."
